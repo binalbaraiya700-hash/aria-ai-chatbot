cf331baf9e9f4c6d3ae4fa25628fcefa879b1cf4
Binal Baraiya <binalbaraiya700@gmail.com>
2025-11-12 22:11:56 +0530

    Add: DB auto-migration to add missing user columns on startup

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ ADMIN_EMAILS = ['binalbaraiya700@gmail.com']
 
 # Initialize extensions
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
